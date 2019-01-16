df2efe32a6dfd4eba7b332952b71c00368f0e3b1
ivansanchezvera <ivansanchez_1988@hotmail.com>
2019-01-16 17:28:04 -0500

    Controlando controles: Agregado control Spinner y ComboBox

--- src/ec/edu/upse/facsistel/sistemas/progvisual1/s2018p2/ropa/controllers/ControllerListaProducto.java
+++ src/ec/edu/upse/facsistel/sistemas/progvisual1/s2018p2/ropa/controllers/ControllerListaProducto.java
@@ -1,8 +1,15 @@
 package ec.edu.upse.facsistel.sistemas.progvisual1.s2018p2.ropa.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
@@ -10,12 +17,23 @@ import javafx.scene.layout.VBox;
 public class ControllerListaProducto {
 
 	@FXML TilePane tileProductos;
+	@FXML ComboBox<String> cmbTemporada;
 	
 	public void initialize()
 	{
+		cargarComboTemporada();
 		cargarProductos();
 	}
 	
+	private void cargarComboTemporada() {
+		
+		List<String> listaTemporadas = new ArrayList<String>();
+		listaTemporadas.add("Invierno");
+		listaTemporadas.add("Verano");
+		ObservableList<String> listaObservableTemporadas = FXCollections.observableArrayList(listaTemporadas);
+		cmbTemporada.setItems(listaObservableTemporadas);
+	}
+
 	public void cargarProductos()
 	{
 		//Creamos unos productos
@@ -40,10 +58,14 @@ public class ControllerListaProducto {
 		imgProducto.setFitHeight(260);
 		imgProducto.setPreserveRatio(true);
 		Label lblDescripcionProducto = new Label(descripcionProducto);
-		Label lblPrecio = new Label(precioProducto);
+		Label lblPrecio = new Label("$ " + precioProducto);
+		
+		Spinner spnCantidad = new Spinner<>(0, 20, 1);
+		
 		Button btnComprar = new Button("Comprar");
 		
-		tileProducto.getChildren().addAll(imgProducto, lblDescripcionProducto, lblPrecio, btnComprar);
+		tileProducto.getChildren().addAll(imgProducto, lblDescripcionProducto, lblPrecio, spnCantidad, btnComprar);
+		
 //		tileProducto.getChildren().add(imgProducto);
 //		tileProducto.getChildren().add(lblDescripcionProducto);
 //		tileProducto.getChildren().add(lblPrecio);
