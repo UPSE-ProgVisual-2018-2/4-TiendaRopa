b9f0346635fda03724f5b0cf96b6b4dde02a6abf
ivansanchezvera <ivansanchez_1988@hotmail.com>
2019-01-18 16:59:51 -0500

    Media View: Aprendimos el control mediaView para mostrar videos y tocar audio. Teoria y Practica. MediaView requiere de MediaPlayer y Media para funcionar. Pantalla de Login para el proyecto y poner en practica el video

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,10 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/ViewListaProductos.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/ViewIntroTienda.fxml"));
+			Scene scene = new Scene(root,640,360);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/ec/edu/upse/facsistel/sistemas/progvisual1/s2018p2/ropa/controllers/ControllerIntroTienda.java
@@ -0,0 +1,77 @@
+package ec.edu.upse.facsistel.sistemas.progvisual1.s2018p2.ropa.controllers;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class ControllerIntroTienda {
+
+	@FXML MediaView mediaTienda; 
+
+	//Media Player global
+	MediaPlayer mp;
+
+	public ControllerIntroTienda() {
+
+	}
+
+	public void initialize()
+	{
+		//		hacerVentanaEstatica();
+		//cargarVideoPromocionalOnline();
+		cargarVideoPromocionalLocal();
+	}
+
+	private void cargarVideoPromocionalLocal() {
+		File f = new File("resources/videos/fashionIntro.mp4");
+		Media media;
+		try {
+			media = new Media(f.toURI().toURL().toString());
+			mp = new MediaPlayer(media);
+			mediaTienda.setMediaPlayer(mp);
+			mp.play();
+			mp.setAutoPlay(true);
+			//Esta linea es para que toque para siempre
+			mp.setCycleCount(MediaPlayer.INDEFINITE);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	//	private void hacerVentanaEstatica() {
+	//		Stage s = (Stage) mediaTienda.getScene().getWindow();
+	//		s.setResizable(false);
+	//	}
+
+	private void cargarVideoPromocionalOnline() {
+
+		Media media = new Media("https://gcs-vimeo.akamaized.net/exp=1547854732~acl=%2A%2F644267110.mp4%2A~hmac=6f018270e3105d385ee81006175b879c1ae2172f16d0fd00429ea40462f7f035/vimeo-prod-skyfire-std-us/01/3606/7/193033143/644267110.mp4");
+		mp = new MediaPlayer(media);
+		mediaTienda.setMediaPlayer(mp);
+		mp.play();
+		mp.setAutoPlay(true);
+	}
+
+	public void play()
+	{
+		mp.play();
+	}
+
+	public void stop()
+	{
+		mp.stop();
+	}
+
+	public void pause()
+	{
+		mp.pause();
+	}
+
+}
